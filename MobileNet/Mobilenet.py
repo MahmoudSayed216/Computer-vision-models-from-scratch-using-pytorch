4dc4c88cb7680bd9654717338ef1d22d4b69ae8c
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-12-27 18:41:04 +0200

    Update Mobilenet.py

--- MobileNet/Mobilenet.py
+++ MobileNet/Mobilenet.py
@@ -6,32 +6,30 @@ print("Hello from the MahmoudNet community")
 
 
 class _conv_block(nn.Module):
-    def __init__(self, in_channels, out_channels, a, p):
+    def __init__(self, in_channels, out_channels, a):
         super().__init__()
         self.a = a
-        self.p = p
 
         out_channels = int(a*out_channels)
-        self.CB = nn.Sequential(
+        self.cb = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 2, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
         )
 
     def forward(self, x):
-        x = self.CB(x)
+        x = self.cb(x)
         return x
 
 
 class _depthwise_separable_convolution(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, a, p):
+    def __init__(self, in_channels, out_channels, stride, a):
         super().__init__()
         self.a = a
-        self.p = p
 
         out_channels = int(a*out_channels)
         in_channels = int(a*in_channels)
-        self.layer = nn.Sequential(
+        self.dwsc_layer = nn.Sequential(
             nn.Conv2d(in_channels, in_channels, 3, stride, padding=1, groups=in_channels),
             nn.BatchNorm2d(in_channels),
             nn.ReLU(),
@@ -42,56 +40,47 @@ class _depthwise_separable_convolution(nn.Module):
         
 
     def forward(self, x):
-        x = self.layer(x)
+        x = self.dwsc_layer(x)
 
         return x
 
         
 class MobileNet(nn.Module):
-    def __init__(self, input_channels: int, depth_multiplier: float, input_size: float):
+    def __init__(self, input_channels: int, depth_multiplier: float, resolution_multiplier: float):
         super().__init__()
         self.base_model_layers_sizes = [32, 64, 128, 128, 256, 256, *[512 for i in range(5)], 512, 1024]
         self.strides = [1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2]
         self.n_layers = 12
         self.a = depth_multiplier
-        self.p = input_size/224
+        self.input_size = int(224 * resolution_multiplier)
         self.input_channels = input_channels
         # self.n_classes = n_classes
         self.dwsc = []
         flattened_size = int(1024*self.a)
-
-        self.ConvBlock1 = _conv_block(self.input_channels, self.base_model_layers_sizes[0], self.a, self.p)
+        self.ConvBlock1 = _conv_block(self.input_channels, self.base_model_layers_sizes[0], self.a)
         self.dwsc = nn.ModuleList([
             _depthwise_separable_convolution(
                 self.base_model_layers_sizes[i], 
                 self.base_model_layers_sizes[i+1], 
                 self.strides[i], 
                 self.a,
-                self.p
             ) for i in range(self.n_layers)
         ])
-        self.Pool = nn.AvgPool2d()
+        self.Pool = nn.AdaptiveAvgPool2d(1)
         self.Flattener = nn.Flatten()
         self.Linear1 = nn.Linear(flattened_size, 1000)
         self.Linear2 = nn.Linear(1000, 1)
         self.Output = nn.Sigmoid()
-
-
+        self.resizer = T.transforms.Resize((self.input_size, self.input_size))
 
     def forward(self, x):
+        x = self.resizer(x)
         x = self.ConvBlock1(x)
-        print(x.shape)
         for i in range(self.n_layers):
             x = self.dwsc[i](x)
-            print(x.shape)
-        print('right before pooling')
         x = self.Pool(x)
-        print('right after pooling',x.shape)
         x = self.Flattener(x)
         x = self.Linear1(x)
-        print(x.shape)
         x = self.Linear2(x)
-        print(x.shape)
         x = self.Output(x)
-        print('separator')
         return x
