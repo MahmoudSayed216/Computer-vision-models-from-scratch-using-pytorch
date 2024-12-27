0b3e3b206ab677aebf56322787c8b5a06673ebab
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-12-27 18:11:58 +0300

    Update Mobilenet.py

--- MobileNet/Mobilenet.py
+++ MobileNet/Mobilenet.py
@@ -32,7 +32,7 @@ class _depthwise_separable_convolution(nn.Module):
         out_channels = int(a*out_channels)
         in_channels = int(a*in_channels)
         self.layer = nn.Sequential(
-            nn.Conv2d(in_channels, in_channels, 3, stride, padding='same' if stride==1 else 'valid', groups=in_channels),
+            nn.Conv2d(in_channels, in_channels, 3, stride, padding=1, groups=in_channels),
             nn.BatchNorm2d(in_channels),
             nn.ReLU(),
             nn.Conv2d(in_channels, out_channels, 1),
@@ -42,9 +42,9 @@ class _depthwise_separable_convolution(nn.Module):
         
 
     def forward(self, x):
-        # old_size = x.shape[2]
-        # new_size = int(old_size* self.p)
-        # resizer = T.Resize((new_size, new_size))
+        old_size = x.shape[2]
+        new_size = int(old_size* self.p)
+        resizer = T.Resize((new_size, new_size))
         x = self.layer(x)
 
         return x
@@ -53,9 +53,9 @@ class _depthwise_separable_convolution(nn.Module):
 class MobileNet(nn.Module):
     def __init__(self, input_channels: int, depth_multiplier: float, input_size: float):
         super().__init__()
-        self.base_model_layers_sizes = [32, 64, 128, 128, 256, 256, *[512 for i in range(5)], 512, 1024, 1024]
-        self.strides = [1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 2]
-        self.n_layers = 13
+        self.base_model_layers_sizes = [32, 64, 128, 128, 256, 256, *[512 for i in range(5)], 512, 1024]
+        self.strides = [1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2]
+        self.n_layers = 12
         self.a = depth_multiplier
         self.p = input_size/224
         self.input_channels = input_channels
@@ -73,9 +73,9 @@ class MobileNet(nn.Module):
                 self.p
             ) for i in range(self.n_layers)
         ])
-        self.Pool = nn.AdaptiveAvgPool2d(7)
+        self.Pool = nn.AvgPool2d(7)
         self.Flattener = nn.Flatten()
-        self.Linear1 = nn.Linear(7*7*flattened_size, 1000)
+        self.Linear1 = nn.Linear(flattened_size, 1000)
         self.Linear2 = nn.Linear(1000, 1)
         self.Output = nn.Sigmoid()
 
@@ -90,7 +90,4 @@ class MobileNet(nn.Module):
         x = self.Linear1(x)
         x = self.Linear2(x)
         x = self.Output(x)
-
         return x
-
-
